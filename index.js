24d1c68173b132f8c5663db7ae53c26ce8bc9589
lizaafrin <lizaafrin446@gmail.com>
2023-05-20 00:01:59 +0600

    Deployed at vercel

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 const express = require("express");
-const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors");
-const port = process.env.Port || 4000;
+require('dotenv').config();
+const app = express();
+const port = process.env.Port || 5000;
 
-const toyInfo = require("./data/toys.json");
+// Middlewares
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send(toyInfo);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qupsx4j.mongodb.net/?retryWrites=true&w=majority`;
+
+// Created a MongoClient object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const toyCollections = client.db("toyCollection").collection("allToys");
+        // console.log(toyCollections);
+
+        app.get('/toys', async (req, res) => {
+            const cursor = toyCollections.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get('/sciencekits', async (req, res) => {
+            const cursor = toyCollections.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+      }
+}
+run().catch(console.dir);
+
+app.get('/', async (req, res) => {
+    res.send("EduJoyToy Server is running");
+})
 
 app.listen(port, () => {
     console.log(`My toy website is Running on Port: ${port}`);
