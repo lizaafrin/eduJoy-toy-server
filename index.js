30af5b5d2873e4ebcab6d9ec9af41c5a8c8d537b
lizaafrin <lizaafrin446@gmail.com>
2023-05-21 11:01:11 +0600

    Route for specific toy added

--- index.js
+++ index.js
@@ -24,15 +24,31 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        await client.connect();
 
         const toyCollections = client.db("toyCollection").collection("allToys");
         // console.log(toyCollections);
 
+        // to get all data from server.
         app.get('/toys', async (req, res) => {
             const cursor = toyCollections.find();
             const result = await cursor.toArray();
             res.send(result);
+        })
+        // to get data for all toy page. Data is limited to 20
+        app.get('/limitedtoys', async (req, res) => {
+            const cursor = toyCollections.find().limit(20);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        // To get data for single toy details.
+        app.get('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: new ObjectId(id) };
+            const result = await toyCollections.findOne(query);
+            res.send(result);
+            // console.log(result);
         })
         app.get('/sciencekits', async (req, res) => {
             const cursor = toyCollections.find();
@@ -40,8 +56,8 @@ async function run() {
             res.send(result);
         })
 
-        // await client.db("admin").command({ ping: 1 });
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
